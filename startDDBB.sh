3bdbafbb5ddcf29481baa7e8967fc29fce754cf3
pablo.calvo <pablo.calvo@babel.es>
2020-06-07 21:53:33 +0200

    modify name and database in start BBDD

--- startDDBB.sh
+++ startDDBB.sh
@@ -1,1 +1,1 @@
-sudo docker run --name some-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=test -d mysql:5.7
+sudo docker run --name db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=blog -d mysql:5.5.62
