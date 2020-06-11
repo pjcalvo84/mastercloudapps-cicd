f785a25f5e80932e6b14ee082e244624eb42b369
pablo.calvo <pablo.calvo@babel.es>
2020-06-11 14:38:13 +0200

    fix

--- src/test/java/es/codeurjc/daw/Constant.java
+++ src/test/java/es/codeurjc/daw/Constant.java
@@ -7,13 +7,9 @@ public class Constant {
     public static final String AUTHOR = "author";
     public static final String MESSAGE = "message";
     public static final String COMMENTS = "comments";
-    public static final String MI_TITULO = "Mi titulo";
-    public static final String MI_CONTENIDO = "Mi contenido";
-    public static final String TITLE = "title";
     public static final String CONTENT = "content";
     public static final String FORM = "form";
     public static final String NUEVO_POST = "Nuevo post";
     public static final String JUAN = "Juan";
     public static final String BUEN_COMENTARIO = "Buen comentario";
-    public static final String AUTHOR = "author";
 }
