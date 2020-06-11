081777efba6e563fdd3e711b84a2d9c9470ee336
pablo.calvo <pablo.calvo@babel.es>
2020-06-11 14:56:32 +0200

    add comment in code for sonar failure

--- src/main/java/es/codeurjc/daw/PostService.java
+++ src/main/java/es/codeurjc/daw/PostService.java
@@ -1,17 +1,12 @@
 package es.codeurjc.daw;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
-
 import es.codeurjc.daw.repository.CommentRepository;
 import es.codeurjc.daw.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -37,6 +32,11 @@ public class PostService {
 	public Comment addComment(long postId, Comment comment) {
 		Post post = postRepository.findById(postId).orElseThrow(EntityNotFoundException::new);
 
+		/*
+
+		Comentario para que salte sonar
+
+		 */
 		Comment savedComment = commentRepository.save(comment);
 
 		post.getComments().add(savedComment);
