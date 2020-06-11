fe6b6917553c735255c3d93908ea2aa926b784ff
pablo.calvo <pablo.calvo@babel.es>
2020-06-11 12:59:35 +0200

    extract constant

---
+++ src/test/java/es/codeurjc/daw/Constant.java
@@ -0,0 +1,9 @@
+package es.codeurjc.daw;
+
+public class Constant {
+    public static final String MI_TITULO = "Mi titulo";
+    public static final String MI_CONTENIDO = "Mi contenido";
+    public static final String TITLE = "title";
+    public static final String AUTHOR = "author";
+    public static final String MESSAGE = "message";
+}
